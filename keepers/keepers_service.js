97f3fef955227ac4ad9eb4a15c0df4fc520aa3a8
thecryptofruit <mango.the.cryptofruit@gmail.com>
2020-07-04 17:19:38 +0200

    basic version of scheduled txs

--- keepers/keepers_service.js
+++ keepers/keepers_service.js
@@ -100,7 +100,7 @@ async function loadLogsOrders() {
 	        arrSupplyIds.push(log.args._tokenIdSupply.toString());
 	    })
 	})	
-	console.log("Loaded", arrSupplyIds.length, " events LogOrderCreated.\n");
+	console.log("Loaded", arrSupplyIds.length, "events LogOrderCreated.\n");
 }
 
 
@@ -120,7 +120,7 @@ async function loadDummyVoucherRedeem() {
 	                arrVouchers[0][0] //_tokenIdVoucher
 	        );
 
-	console.log("Redeem TX hash: ", tx.hash);	
+	console.log("Redeem", 0, " TX hash: ", tx.hash);	
 }
 
 
@@ -128,64 +128,175 @@ async function loadLogsVoucherDeliveredEvents() {
 	var filter = contractCashier.filters.LogVoucherDelivered();
 	var logs = await contractCashier.queryFilter(filter, 0, "latest").then((logs) => {
 	    logs.forEach((log) => {
-	        arrVouchers.push([log.args._tokenIdVoucher.toString(), 'committed']);
+	        arrVouchers.push([log.args._tokenIdVoucher.toString(), 'commitment']);
 	    })
 	})	
-	console.log("Loaded", arrVouchers.length, " events LogVoucherDelivered.\n");
+	console.log("Loaded", arrVouchers.length, "events LogVoucherDelivered.\n");
 }
 
 
-async function loadLogsVoucherRedeemedEvents() {
-	var cntRedeemed = 0;
+async function loadLogsVoucherEvents() {
+	//redemptions
+	var cnt = 0;
 	var filter = contractVK.filters.LogVoucherRedeemed();
 	var logs = await contractVK.queryFilter(filter, 0, "latest").then((logs) => {
 	    logs.forEach((log) => {
 	        for (let i = 0; i < arrVouchers.length; i++) {
 	        	if (arrVouchers[i][0] == log.args._tokenIdVoucher) {
-	        		arrVouchers[i][1] = 'redeemed';
-	        		cntRedeemed += 1;
+	        		arrVouchers[i][1] = 'redemption';
+	        		cnt += 1;
 	        	}
 	        }
-	        
 	    })
+		console.log("Loaded", cnt, "events LogVoucherRedeemed.\n");
 	})	
-	console.log("Loaded", cntRedeemed, " events LogVoucherDelivered.\n");	
+
+	//refunds
+	cnt = 0;
+	var filter = contractVK.filters.LogVoucherRefunded();
+	var logs = await contractVK.queryFilter(filter, 0, "latest").then((logs) => {
+	    logs.forEach((log) => {
+	        for (let i = 0; i < arrVouchers.length; i++) {
+	        	if (arrVouchers[i][0] == log.args._tokenIdVoucher) {
+	        		arrVouchers[i][1] = 'refund';
+	        		cnt += 1;
+	        	}
+	        }	        
+	    })
+	    console.log("Loaded", cnt, "events LogVoucherRefunded.\n");	
+	})	
+
+	//expirations
+	cnt = 0;
+	var filter = contractVK.filters.LogExpirationTriggered();
+	var logs = await contractVK.queryFilter(filter, 0, "latest").then((logs) => {
+	    logs.forEach((log) => {
+	        for (let i = 0; i < arrVouchers.length; i++) {
+	        	if (arrVouchers[i][0] == log.args._tokenIdVoucher) {
+	        		arrVouchers[i][1] = 'expiration';
+	        		cnt += 1;
+	        	}
+	        }	        
+	    })
+	    console.log("Loaded", cnt, "events LogExpirationTriggered.\n");	
+	})	
+
+	//finalized
+	cnt = 0;
+	var filter = contractVK.filters.LogFinalizeVoucher();
+	var logs = await contractVK.queryFilter(filter, 0, "latest").then((logs) => {
+	    logs.forEach((log) => {
+	        for (let i = 0; i < arrVouchers.length; i++) {
+	        	if (arrVouchers[i][0] == log.args._tokenIdVoucher) {
+	        		arrVouchers[i][1] = 'final';
+	        		cnt += 1;
+	        	}
+	        }	        
+	    })
+	    console.log("Loaded", cnt, "events LogFinalizeVoucher.\n");	
+	})		
+
+	//funds released
+	var cntP = 0;
+	var cntD = 0;
+	var filter = contractVK.filters.LogFundsReleased();
+	var logs = await contractVK.queryFilter(filter, 0, "latest").then((logs) => {
+	    logs.forEach((log) => {
+	        for (let i = 0; i < arrVouchers.length; i++) {
+	        	if (arrVouchers[i][0] == log.args._tokenIdVoucher) {
+	        		if (log.args._type == 0) {
+		        		if (arrVouchers[i][1] == 'final') {
+		        			arrVouchers[i][1] = 'releasedPayment';	
+		        		} else {
+		        			arrVouchers[i][1] = 'released';	
+		        		}	
+		        		cntP += 1;
+		        	} else if (log.args._type == 1)  {
+		        		if (arrVouchers[i][1] == 'final') {
+		        			arrVouchers[i][1] = 'releasedDeposit';	
+		        		} else {
+		        			arrVouchers[i][1] = 'released';	
+		        		}	
+		        		cntD += 1;
+		        	} else {
+		        		console.log("LogFundsReleased unexpected type: ", log.args._type, " for ", log.args._tokenIdVoucher);
+		        	}   			        	
+	        	}
+	        }	        
+	    })
+		console.log("Loaded", cntP, "events LogFundsReleased(_,0).\n");
+		console.log("Loaded", cntD, "events LogFundsReleased(_,1).\n");	    
+	})	
 }
 
 
+async function triggerExpirations() {
+	for (let i = 0; i < arrVouchers.length; i++) {
+		if (arrVouchers[i][1] == 'commitment') {
+			tx = await contractVK.triggerExpiration(
+			                arrVouchers[0][0] //_tokenIdVoucher
+			        );
+
+			console.log("Expire", i, "TX hash: ", tx.hash);			
+		}
+	}
+}
+
+
+async function triggerFinalizations() {
+	for (let i = 0; i < arrVouchers.length; i++) {
+		if (arrVouchers[i][1] == 'commitment' 
+			|| arrVouchers[i][1] == 'redemption' 
+			|| arrVouchers[i][1] == 'refund') 
+		{
+			tx = await contractVK.triggerExpiration(
+			                arrVouchers[0][0] //_tokenIdVoucher
+			        );
+
+			console.log("Finalize", i, "TX hash: ", tx.hash);			
+		}
+	}
+}
+
+
+async function triggerWithdrawals() {
+	for (let i = 0; i < arrVouchers.length; i++) {
+		if (arrVouchers[i][1] == 'commitment' 
+			|| arrVouchers[i][1] == 'redemption' 
+			|| arrVouchers[i][1] == 'refund' 
+			|| arrVouchers[i][1] == 'expiration' 
+			|| arrVouchers[i][1] == 'final') 
+		{
+			tx = await contractCashier.withdraw(
+			                [arrVouchers[0][0]] //_tokenIdVoucher
+			        );
+
+			console.log("Withdrawal", i, "TX hash: ", tx.hash);			
+		}
+	}
+}
+
 
 async function doStuff() {
+	console.log("\nKeepers start ...", new Date().toUTCString(), "\n");
 	await initContracts();
+
 	await loadDummyOrders();
 	await loadLogsOrders();	
 	await loadDummyVoucherCommitments();
 	await loadLogsVoucherDeliveredEvents();
 	await loadDummyVoucherRedeem();
-	await loadLogsVoucherRedeemedEvents();
 	
+	await loadLogsVoucherEvents();
+	
+	//actually do txs
+	await triggerExpirations();
+	await triggerFinalizations();
+	await triggerWithdrawals();
+	
+	console.log("\nKeepers end ...", new Date().toUTCString(), "\n");
 }
 
 
 
 doStuff();
-
-
-// //listen for new offers
-// contractCashier.on("LogOrderCreated", (_tokenIdSupply, _seller, _promiseId, _quantity, event) => {
-// });
-
-
-// //listen for new purchases
-// contractCashier.on("LogVoucherDelivered", (_tokenIdSupply, _tokenIdVoucher, _issuer, _holder, _promiseId, event) => {
-// });
-
-
-//actions
-/*
-VoucherKernel:
-- triggerExpiration(uint256 _tokenIdVoucher)
-- triggerFinalizeVoucher(uint256 _tokenIdVoucher)
-
-Cashier:
-- withdraw(uint256[] calldata _tokenIdVouchers)
-*/
