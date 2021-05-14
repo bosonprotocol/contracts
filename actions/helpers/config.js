d362042da88f8bf2c2c3621c5c79b22c559aceb5
srihari kapu <sriharikapu163@gmail.com>
2021-05-14 12:21:46 +0530

    update config

---
+++ actions/helpers/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    TOKEN_SUPPLY_ID: "token supply id",
+    VOUCHER_ID: "voucher id",
+contracts: {
+        BosonRouterContrctAddress: '0xAcaC4421ab76B71b195A8d70F9Cab6a23CD44cF4' // rinkeby boson router testnet address
+},
+    DEPLOYER_PUBLIC: 'your deployer address',
+    DEPLOYER_SECRET: 'your deployer secret',
+    BUYER_PUBLIC: 'your buyer address',
+    BUYER_SECRET: 'your buyer private key / secret',
+    SELLER_PUBLIC: 'your seller address',
+    SELLER_SECRET: 'your seller private key / secret',
+    // PROVIDER: 'http://localhost:8545', // for local ganache
+    PROVIDER: "https://rinkeby.infura.io/v3/yourinfurakey" //https://rinkeby.infura.io/v3/your infura key
+}
+
+
