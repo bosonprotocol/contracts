78cc2a2a55abe39ade9c2b6be4601190f4c12a08
zajck <klemen@impressieve.com>
2021-09-20 10:45:32 +0200

    non-transfer functionality

--- test/8_erc1155_non_transferable.ts
+++ test/8_erc1155_non_transferable.ts
@@ -1,7 +1,7 @@
-import { ethers } from 'hardhat';
-import { Signer, ContractFactory, Contract } from 'ethers';
+import {ethers} from 'hardhat';
+import {Signer, ContractFactory, Contract} from 'ethers';
 
-import { assert, expect } from 'chai';
+import {assert, expect} from 'chai';
 import constants from '../testHelpers/constants';
 
 import Users from '../testHelpers/users';
@@ -9,15 +9,13 @@ import Utils from '../testHelpers/utils';
 
 const BN = ethers.BigNumber.from;
 
-import {
-  ERC1155NonTransferable,
-} from '../typechain';
+import {ERC1155NonTransferable} from '../typechain';
 
 let ERC1155NonTransferable_Factory: ContractFactory;
 
 import revertReasons from '../testHelpers/revertReasons';
 import * as eventUtils from '../testHelpers/events';
-import { eventNames } from '../testHelpers/events';
+import {eventNames} from '../testHelpers/events';
 
 let users;
 
@@ -29,7 +27,6 @@ describe('ERC1155 non transferable functionality', async () => {
     ERC1155NonTransferable_Factory = await ethers.getContractFactory(
       'ERC1155NonTransferable'
     );
-
   });
 
   let contractERC1155NonTransferable: ERC1155NonTransferable;
@@ -75,7 +72,12 @@ describe('ERC1155 non transferable functionality', async () => {
 
     it('Owner should be able to mint batch', async () => {
       const nftTokenIDs = [BN('2'), BN('5'), BN('7'), BN('9')];
-      const balances = [constants.ONE, constants.ONE, constants.ONE, constants.ONE]
+      const balances = [
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+      ];
       await contractERC1155NonTransferable.mintBatch(
         users.other1.address,
         nftTokenIDs,
@@ -84,17 +86,21 @@ describe('ERC1155 non transferable functionality', async () => {
       );
 
       const balance = await contractERC1155NonTransferable.balanceOfBatch(
-        [users.other1.address, users.other1.address, users.other1.address, users.other1.address],
+        [
+          users.other1.address,
+          users.other1.address,
+          users.other1.address,
+          users.other1.address,
+        ],
         nftTokenIDs
       );
 
       assert.equal(
-        JSON.stringify(balance.map(balance => balance.toString())),
-        JSON.stringify(balances.map(balance => balance.toString())),
+        JSON.stringify(balance.map((balance) => balance.toString())),
+        JSON.stringify(balances.map((balance) => balance.toString())),
         'Balance mismatch'
       );
     });
-
 
     it('Owner should be able to burn', async () => {
       const nftTokenID = BN('2');
@@ -108,7 +114,7 @@ describe('ERC1155 non transferable functionality', async () => {
       await contractERC1155NonTransferable.burn(
         users.other1.address,
         nftTokenID,
-        constants.ONE,
+        constants.ONE
       );
 
       const balance = await contractERC1155NonTransferable.balanceOf(
@@ -123,11 +129,20 @@ describe('ERC1155 non transferable functionality', async () => {
       );
     });
 
-
     it('Owner should be able to mint batch', async () => {
       const nftTokenIDs = [BN('2'), BN('5'), BN('7'), BN('9')];
-      const balances = [constants.ONE, constants.ONE, constants.ONE, constants.ONE]
-      const zeroBalances = [constants.ZERO, constants.ZERO, constants.ZERO, constants.ZERO]
+      const balances = [
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+      ];
+      const zeroBalances = [
+        constants.ZERO,
+        constants.ZERO,
+        constants.ZERO,
+        constants.ZERO,
+      ];
 
       await contractERC1155NonTransferable.mintBatch(
         users.other1.address,
@@ -143,14 +158,65 @@ describe('ERC1155 non transferable functionality', async () => {
       );
 
       const balance = await contractERC1155NonTransferable.balanceOfBatch(
-        [users.other1.address, users.other1.address, users.other1.address, users.other1.address],
+        [
+          users.other1.address,
+          users.other1.address,
+          users.other1.address,
+          users.other1.address,
+        ],
         nftTokenIDs
       );
 
       assert.equal(
-        JSON.stringify(balance.map(balance => balance.toString())),
-        JSON.stringify(zeroBalances.map(balance => balance.toString())),
+        JSON.stringify(balance.map((balance) => balance.toString())),
+        JSON.stringify(zeroBalances.map((balance) => balance.toString())),
         'Balance mismatch'
+      );
+    });
+
+    it('Tokens are non-transferable', async () => {
+      const nftTokenID = BN('2');
+      await contractERC1155NonTransferable.mint(
+        users.other1.address,
+        nftTokenID,
+        constants.ONE,
+        constants.ZERO_BYTES
+      );
+
+      const ownerInstance = contractERC1155NonTransferable.connect(
+        users.other1.signer
+      );
+
+      await expect(
+        ownerInstance.safeTransferFrom(
+          users.other1.address,
+          users.other2.address,
+          nftTokenID,
+          constants.ONE,
+          constants.ZERO_BYTES
+        )
+      ).to.be.revertedWith(revertReasons.NON_TRANSFERABLE);
+
+      const balance1 = await contractERC1155NonTransferable.balanceOf(
+        users.other1.address,
+        nftTokenID
+      );
+
+      const balance2 = await contractERC1155NonTransferable.balanceOf(
+        users.other2.address,
+        nftTokenID
+      );
+
+      assert.equal(
+        balance1.toString(),
+        constants.ONE.toString(),
+        'Balance of user 1mismatch'
+      );
+
+      assert.equal(
+        balance2.toString(),
+        constants.ZERO.toString(),
+        'Balance of user 2 mismatch'
       );
     });
 
@@ -182,7 +248,6 @@ describe('ERC1155 non transferable functionality', async () => {
       ).to.be.revertedWith(revertReasons.UNAUTHORIZED_OWNER);
     });
 
-
     it('[NEGATIVE][burn] Should revert if executed by attacker', async () => {
       const attackerInstance = contractERC1155NonTransferable.connect(
         users.attacker.signer
@@ -197,11 +262,7 @@ describe('ERC1155 non transferable functionality', async () => {
       );
 
       await expect(
-        attackerInstance.burn(
-          users.other1.address,
-          nftTokenID,
-          constants.ONE,
-        )
+        attackerInstance.burn(users.other1.address, nftTokenID, constants.ONE)
       ).to.be.revertedWith(revertReasons.UNAUTHORIZED_OWNER);
     });
 
@@ -211,7 +272,12 @@ describe('ERC1155 non transferable functionality', async () => {
       );
 
       const nftTokenIDs = [BN('2'), BN('5'), BN('7'), BN('9')];
-      const balances = [constants.ONE, constants.ONE, constants.ONE, constants.ONE]
+      const balances = [
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+      ];
       await contractERC1155NonTransferable.mintBatch(
         users.other1.address,
         nftTokenIDs,
@@ -220,11 +286,7 @@ describe('ERC1155 non transferable functionality', async () => {
       );
 
       await expect(
-        attackerInstance.burnBatch(
-          users.other1.address,
-          nftTokenIDs,
-          balances,
-        )
+        attackerInstance.burnBatch(users.other1.address, nftTokenIDs, balances)
       ).to.be.revertedWith(revertReasons.UNAUTHORIZED_OWNER);
     });
 
@@ -250,33 +312,21 @@ describe('ERC1155 non transferable functionality', async () => {
     });
 
     it('Owner should be able to pause', async () => {
-
       await contractERC1155NonTransferable.pause();
 
-      const status = await contractERC1155NonTransferable.paused(
-      );
+      const status = await contractERC1155NonTransferable.paused();
 
-      assert.equal(
-        status,
-        true,
-        'Should be paused'
-      );
+      assert.equal(status, true, 'Should be paused');
     });
 
     it('Owner should be able to unpause', async () => {
-
       await contractERC1155NonTransferable.pause();
 
       await contractERC1155NonTransferable.unpause();
 
-      const status = await contractERC1155NonTransferable.paused(
-      );
+      const status = await contractERC1155NonTransferable.paused();
 
-      assert.equal(
-        status,
-        false,
-        'Should be unpaused'
-      );
+      assert.equal(status, false, 'Should be unpaused');
     });
 
     it('[NEGATIVE] During the pause mint and burn does not work', async () => {
@@ -285,7 +335,12 @@ describe('ERC1155 non transferable functionality', async () => {
       const nftTokenID = BN('2');
 
       const nftTokenIDs = [BN('2'), BN('5'), BN('7'), BN('9')];
-      const balances = [constants.ONE, constants.ONE, constants.ONE, constants.ONE]
+      const balances = [
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+        constants.ONE,
+      ];
       // const zeroBalances = [constants.ZERO, constants.ZERO, constants.ZERO, constants.ZERO]
 
       await expect(
@@ -301,7 +356,7 @@ describe('ERC1155 non transferable functionality', async () => {
         contractERC1155NonTransferable.burn(
           users.other1.address,
           nftTokenID,
-          constants.ONE,
+          constants.ONE
         )
       ).to.be.revertedWith(revertReasons.PAUSED_ERC1155);
 
@@ -327,23 +382,20 @@ describe('ERC1155 non transferable functionality', async () => {
       const attackerInstance = contractERC1155NonTransferable.connect(
         users.attacker.signer
       );
-      await expect(
-        attackerInstance.pause()
-      ).to.be.revertedWith(revertReasons.UNAUTHORIZED_OWNER);
+      await expect(attackerInstance.pause()).to.be.revertedWith(
+        revertReasons.UNAUTHORIZED_OWNER
+      );
     });
 
     it('[NEGATIVE][unpause] Should revert if executed by attacker', async () => {
       await contractERC1155NonTransferable.pause();
-      
+
       const attackerInstance = contractERC1155NonTransferable.connect(
         users.attacker.signer
       );
-      await expect(
-        attackerInstance.unpause()
-      ).to.be.revertedWith(revertReasons.UNAUTHORIZED_OWNER);
+      await expect(attackerInstance.unpause()).to.be.revertedWith(
+        revertReasons.UNAUTHORIZED_OWNER
+      );
     });
-
   });
-
-
 });
