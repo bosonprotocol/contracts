61081ffc167e54ffa017a47c2ed45f2827f356f4
zajck <klemen@impressieve.com>
2021-09-28 10:37:00 +0200

    removed unnecessary copies

--- contracts/interfaces/IERC20WithPermit.sol
+++ contracts/interfaces/IERC20WithPermit.sol
@@ -1,40 +1,16 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity 0.7.6;
 
-interface IERC20WithPermit {
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-    event Transfer(address indexed from, address indexed to, uint256 value);
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+interface IERC20WithPermit is IERC20 {
     function name() external view returns (string memory);
 
     function symbol() external view returns (string memory);
 
     function decimals() external pure returns (uint8);
 
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address owner) external view returns (uint256);
-
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    function approve(address spender, uint256 value) external returns (bool);
-
-    function transfer(address to, uint256 value) external returns (bool);
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) external returns (bool);
-
-    // solhint-disable-next-line func-name-mixedcase
+      // solhint-disable-next-line func-name-mixedcase
     function DOMAIN_SEPARATOR() external view returns (bytes32);
 
     // solhint-disable-next-line func-name-mixedcase
