080215c48614df9188dd78672f691fb76a086aa9
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2021-10-14 19:47:57 +0530

    added new mock: MockERC1155.sol

---
+++ contracts/mocks/MockERC1155.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MockERC1155 is ERC1155 {
+    constructor() ERC1155("https://mock.example/{id}.json") {
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external pure returns(bytes4) {
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
+}
