d41b2dd641868e35d0e6f45a8d85942d3cf239f3
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2021-10-14 19:05:16 +0530

    Implemented onERC721Received

--- contracts/mocks/MockERC721.sol
+++ contracts/mocks/MockERC721.sol
@@ -2,8 +2,13 @@
 pragma solidity 0.7.6;
  
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
- 
-contract MockERC721 is ERC721 {
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract MockERC721 is ERC721, IERC721Receiver {
   constructor() ERC721("MockERC721", "M721") {
+  }
+
+  function onERC721Received(address, address, uint256, bytes calldata) external override pure returns(bytes4) {
+    return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
   }
 }
