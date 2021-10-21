824c971b387d23fd5f92a9d67a44e920d96e7691
cliffhall <cliff@futurescale.com>
2021-10-21 12:28:11 -0400

    Remediate BRR-03C
    * In BosonRouter.sol,
      - in checkLimits method
        - changed location of _metadata argument from memory to calldata

--- contracts/BosonRouter.sol
+++ contracts/BosonRouter.sol
@@ -959,7 +959,7 @@ contract BosonRouter is
      * @param _tokensSent     tokens sent to cashier contract
      */
     function checkLimits(
-        uint256[] memory _metadata,
+        uint256[] calldata _metadata,
         address _tokenPriceAddress,
         address _tokenDepositAddress,
         uint256 _tokensSent
