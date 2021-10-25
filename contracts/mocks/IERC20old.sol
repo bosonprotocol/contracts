c2fa335cc3e22b8154314646d68a7fc79ccd4eac
zajck <klemen@impressieve.com>
2021-10-25 09:29:24 +0200

    format fix

--- contracts/mocks/IERC20old.sol
+++ contracts/mocks/IERC20old.sol
@@ -2,6 +2,10 @@
 pragma solidity 0.7.6;
 
 // some old ERC20 implementations do not return (bool) when transferFrom is called
-interface IERC20Old  {
-    function transferFrom(address from, address to, uint value) external;
+interface IERC20Old {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external;
 }
