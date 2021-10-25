e1ec4953b348f2bc545c091d7cccd331e038f471
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2021-10-25 12:49:50 +0530

    added the events emitted during setter function call

--- testHelpers/events.ts
+++ testHelpers/events.ts
@@ -43,6 +43,8 @@ export const eventNames = {
   LOG_URI_SET: 'LogUriSet',
   USED_NONCE: 'UsedNonce',
   EXECUTED_META_TX: 'ExecutedMetaTransaction',
+  LOG_VOUCHER_KERNEL_SET: 'LogVoucherKernelSet',
+  LOG_TOKEN_REGISTRY_SET: 'LogTokenRegistrySet',
 };
 
 import {ContractFactory, ContractReceipt} from 'ethers';
