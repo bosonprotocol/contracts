96354510cada3cffd5848378ab3a731a3bcefde6
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2021-10-27 15:56:14 +0530

    Removed as we do not need to check supportsInterface for ERC-721

--- contracts/mocks/MockERC721Receiver.sol
+++ contracts/mocks/MockERC721Receiver.sol
@@ -2,15 +2,8 @@
 pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
-import "@openzeppelin/contracts/introspection/ERC165.sol";
 
 // solhint-disable-next-line no-empty-blocks
-contract MockERC721Receiver is ERC165, ERC721Holder {
-    // Equals to `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
-    // which can be also obtained as `IERC721Receiver(0).onERC721Received.selector`
-    bytes4 private constant _ERC721_RECEIVED = 0x150b7a02;
+contract MockERC721Receiver is ERC721Holder {
 
-    constructor() {
-        _registerInterface(_ERC721_RECEIVED);
-    }
 }
