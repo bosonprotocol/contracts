7d53fa92bd930fb78f6f8ae3815d2dea39d99a71
zajck <klemen@impressieve.com>
2021-11-01 18:17:03 +0100

    helper to calculate next n contract addreses

---
+++ testHelpers/contractAddress.ts
@@ -0,0 +1,26 @@
+import {ethers} from 'hardhat';
+
+const {keccak256, RLP} = ethers.utils;
+const BN = ethers.BigNumber.from;
+
+export async function calculateDeploymentAddresses(deployer, n) {
+    let addresses = [];
+    let startingNonce = await ethers.provider.getTransactionCount(
+        deployer
+      );
+
+    for (let i = 0; i < n; i++) {
+        const nonce = BN(startingNonce).add(i); 
+        const nonceHex = nonce.eq(0) ? "0x" : nonce.toHexString()
+
+        const input_arr = [ deployer, nonceHex];
+        const rlp_encoded = RLP.encode(input_arr);
+
+        const contract_address_long = keccak256(rlp_encoded);
+
+        const contract_address = "0x" + contract_address_long.substring(26); //Trim the first 24 characters.
+        
+        addresses.push(contract_address)
+        }
+    return addresses;
+  }
