05b66f57c6bde96f67afa3e37017c80061fd969f
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2021-12-17 20:26:43 +0530

    cleanup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -85,8 +85,5 @@ const config: HardhatUserConfig = {
 		outputFile: 'GasReport.txt'
 	}
 };
-console.log(config.gasReporter);
-console.log(`process.env.CMC_API_KEY length is ${process.env.CMC_API_KEY.length}`);
-console.log(`coinmarketcap key length is ${config.gasReporter.coinmarketcap.length}`);
 
 export default config;
