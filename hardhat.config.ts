36b24bd78d6c080af66155ae3f0a26ddc69639c3
bidyut-arianelabs <bidyut.nath@arianelabs.com>
2021-12-17 19:18:03 +0530

    new approach

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -85,5 +85,8 @@ const config: HardhatUserConfig = {
 		outputFile: 'GasReport.txt'
 	}
 };
+console.log(`config.gasReporter is ${config.gasReporter}`);
+console.log(`process.env.CMC_API_KEY length is ${process.env.CMC_API_KEY.length}`);
+console.log(`coinmarketcap key length is ${config.gasReporter.coinmarketcap.length}`);
 
 export default config;
