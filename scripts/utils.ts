eb159d79d6919ad33373ee54ebecc87bf8831cc9
zajck <klemen@impressieve.com>
2022-02-24 09:33:55 +0100

    format

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -12,8 +12,12 @@ export function isValidEnv(env: string): boolean {
   return availableEnvironments.some((e) => e == env);
 }
 
-export function getAddressesFilePath(chainId: number, env?: string, suffix?: string): string {
-  return `${addressesDirPath}/${chainId}${
-    env ? `-${env.toLowerCase()}` : ''
-  }${suffix ? `-${suffix}` : ''}.json`;
+export function getAddressesFilePath(
+  chainId: number,
+  env?: string,
+  suffix?: string
+): string {
+  return `${addressesDirPath}/${chainId}${env ? `-${env.toLowerCase()}` : ''}${
+    suffix ? `-${suffix}` : ''
+  }.json`;
 }
