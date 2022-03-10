9882e51ba3e69b8476abc5f3efdbdcbb5aaf2796
zajck <klemen@impressieve.com>
2022-03-10 11:17:35 +0100

    renamed getTotalDeposits -> getTotalDepositsForVoucher

--- contracts/VoucherKernel.sol
+++ contracts/VoucherKernel.sol
@@ -735,7 +735,7 @@ contract VoucherKernel is IVoucherKernel, Ownable, Pausable, ReentrancyGuard {
 
         vouchersStatus[_tokenIdVoucher].depositReleased.releasedAmount = uint248(uint256(vouchersStatus[_tokenIdVoucher].depositReleased.releasedAmount).add(_amount));
 
-        if (vouchersStatus[_tokenIdVoucher].depositReleased.releasedAmount == getTotalDeposits(_tokenIdVoucher)) {
+        if (vouchersStatus[_tokenIdVoucher].depositReleased.releasedAmount == getTotalDepositsForVoucher(_tokenIdVoucher)) {
             vouchersStatus[_tokenIdVoucher].isDepositsReleased = true;
             emit LogFundsReleased(_tokenIdVoucher, 1); 
         }        
@@ -1033,7 +1033,7 @@ contract VoucherKernel is IVoucherKernel, Ownable, Pausable, ReentrancyGuard {
      * @notice Get the sum of buyer and seller deposit for the voucher
      * @param _tokenIdVoucher   ID of the voucher token
      */
-    function getTotalDeposits(uint256 _tokenIdVoucher)
+    function getTotalDepositsForVoucher(uint256 _tokenIdVoucher)
         internal
         view
         returns (
