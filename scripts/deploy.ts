44a2bcb6d272cb3f37a462d55ef02ec92acd1033
zajck <klemen@impressieve.com>
2022-03-14 10:31:54 +0100

    Warn if erc1155nt address not provided

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -303,7 +303,11 @@ class DeploymentExecutor {
 
     console.log('DAI Token Address Used: ', this.dai_token);
     console.log('Boson Token Address Used: ', this.boson_token);
-    console.log('Erc1155NonTransferable Token Address Used: ', this.erc1155NonTransferable);
+    if (this.erc1155NonTransferable) {
+      console.log('Erc1155NonTransferable Token Address Used: ', this.erc1155NonTransferable)
+    } else {
+      console.warn('Erc1155NonTransferable Token Address not provided!');
+    };
   }
 
   async writeContracts() {
